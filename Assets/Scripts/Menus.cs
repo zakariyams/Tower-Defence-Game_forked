a4032b25dcd552e72d3c796ec8772d3da2a8e616
Zakariya Mohamed Shafe <moza22kw@student.ju.se>
2024-04-06 19:35:39 +0200

    Worked on the menu/shop system

--- Assets/Scripts/Menus.cs
+++ Assets/Scripts/Menus.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Menus : MonoBehaviour
 {
+
+
+    [SerializeField] Animator anime;
+
+    private bool isMenuOpen = true;
+
+
+    public void ToggleMenu()
+    {
+        isMenuOpen = !isMenuOpen;
+        anime.SetBool("MenuOpen", isMenuOpen);
+    }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +31,7 @@ public class Menus : MonoBehaviour
     {
         
     }
+
+
+
 }
