17d8c63e93339e7fb8651def9d3f6764ee3020ab
Jakobloula <113102505+Jakobloula@users.noreply.github.com>
2024-04-07 20:34:03 +0200

    Fixed merge issues

--- Assets/Scripts/TowerScripts/Towers.cs
+++ Assets/Scripts/TowerScripts/Towers.cs
@@ -11,13 +11,9 @@ public abstract class Towers : MonoBehaviour
     public float Bps {  get; protected set; }
     [SerializeField] private LayerMask enemy;
     public GameObject Target { get; protected set; }
-    protected float timeUntilFire;
+    public float timeUntilFire;
 
-<<<<<<< HEAD
-=======
 
-    
->>>>>>> 797a89f33d93ab592a1619be83e61ff716eefa49
     // Finds the closest enemy and places it as the next target. RayCAstHit2D[] is an array of all objects that goes in the circle
     public void FindTarget( )
     {
